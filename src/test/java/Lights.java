d6ce3c1e92910d10c6fb94ff903dcd3b5a18ddf7
yavuzcinar <yvzgzd17022020@gmail.com>
2023-11-04 19:19:08 +0300

    feature light

---
+++ src/test/java/Lights.java
@@ -0,0 +1,12 @@
+public class Lights {
+    String color;
+    String size;
+    String opacity;
+
+    public static void main(String[] args) {
+        Lights light=new Lights();
+        light.color="blue";
+        light.opacity="50";
+        light.size="40'";
+    }
+}
