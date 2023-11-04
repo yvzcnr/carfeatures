87f5c294f5d01d7162d276fa5a6a91c11c2e7003
yavuzcinar <yvzgzd17022020@gmail.com>
2023-11-04 19:09:57 +0300

    added car feature1

---
+++ src/test/java/mamin.java
@@ -0,0 +1,11 @@
+public class mamin {
+    public static void main(String[] args) {
+
+
+    CarFeature1 tire =new CarFeature1();
+    tire.tyre="4";
+    tire.tyrecolor="blue";
+    tire.tyreModel="Yokohama";
+    tire.tyreWidth="18";
+
+} }
